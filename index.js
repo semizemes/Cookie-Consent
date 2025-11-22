c0c6e71f4e95385b41ecde81f04515d0c66c419a
Abbos Soatmurodov <soatmurodovabbos@gmail.com>
2025-11-22 19:26:15 +0500

    feat: add modal close button functionality and update submission handling

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const modal = document.getElementById('modal')
+const modalCloseBtn = document.getElementById('modal-close-btn')
 const consentForm = document.getElementById('consent-form')
 const modalText = document.getElementById('modal-text')
 
@@ -8,6 +9,9 @@ setTimeout(function(){
 
 consentForm.addEventListener('submit', function(e){
     e.preventDefault()
+    
+    const consentFormData = new FormData(consentForm)
+    const fullName = consentFormData.get('fullName')
     
     modalText.innerHTML = `
     <div class="modal-inner-loading">
@@ -19,5 +23,17 @@ consentForm.addEventListener('submit', function(e){
         document.getElementById('upload-text').innerText = `
         Making the sale...`
     }, 1500)
+    
+    
+    setTimeout(function(){
+        document.getElementById('modal-inner').innerHTML = `
+        <h2>Thanks <span class="modal-display-name">${fullName}</span>, you sucker! </h2>
+        <p>We just sold the rights to your eternal soul.</p>
+        <div class="idiot-gif">
+            <img src="images/pirate.gif">
+        </div>
+    `
+    modalCloseBtn.disabled = false
+    }, 3000)
   
 }) 
