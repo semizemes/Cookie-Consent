2d24e75f97aecc0b107cc329a1da0e3ab581ab4a
Abbos Soatmurodov <soatmurodovabbos@gmail.com>
2025-11-22 19:24:38 +0500

    feat: add consent form submission handling and loading modal

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 const modal = document.getElementById('modal')
+const consentForm = document.getElementById('consent-form')
+const modalText = document.getElementById('modal-text')
 
 setTimeout(function(){
     modal.style.display = 'inline'
 }, 1500)
+
+consentForm.addEventListener('submit', function(e){
+    e.preventDefault()
+    
+    modalText.innerHTML = `
+    <div class="modal-inner-loading">
+        <img src="images/loading.svg" class="loading">
+        <p id="upload-text">Uploading your data to the dark web...</p>
+    </div>` 
+    
+    setTimeout(function(){
+        document.getElementById('upload-text').innerText = `
+        Making the sale...`
+    }, 1500)
+  
+}) 
