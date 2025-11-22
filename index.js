e85c475484eb0e049c6f2d3bd2f5314a04739464
Abbos Soatmurodov <soatmurodovabbos@gmail.com>
2025-11-22 19:26:51 +0500

    feat: add reverse button layout for modal choices on decline hover

--- index.js
+++ index.js
@@ -2,10 +2,20 @@ const modal = document.getElementById('modal')
 const modalCloseBtn = document.getElementById('modal-close-btn')
 const consentForm = document.getElementById('consent-form')
 const modalText = document.getElementById('modal-text')
+const declineBtn = document.getElementById('decline-btn')
+const modalChoiceBtns = document.getElementById('modal-choice-btns')
 
 setTimeout(function(){
     modal.style.display = 'inline'
 }, 1500)
+
+modalCloseBtn.addEventListener('click', function(){
+    modal.style.display = 'none'
+})
+
+declineBtn.addEventListener('mouseenter', function(){
+    modalChoiceBtns.classList.toggle('modal-btns-reverse')
+})
 
 consentForm.addEventListener('submit', function(e){
     e.preventDefault()
